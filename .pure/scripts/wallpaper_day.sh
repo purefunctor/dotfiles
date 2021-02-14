69730e85cbd6526e787d00ccecc9fcac84d35f27
PureFunctor <purefunctor@gmail.com>
2021-02-14 20:25:45 +0800

    Make wallpaper scripts run relative to the time

--- .pure/scripts/wallpaper_day.sh
+++ .pure/scripts/wallpaper_day.sh
@@ -1,1 +1,3 @@
-DISPLAY=:0 feh --bg-fill --randomize /home/pure/Wallpapers/Scenery/Day/*
+if [[ $(date +%k) -lt 18 ]]; then
+   DISPLAY=:0 feh --bg-fill --randomize /home/pure/Wallpapers/Scenery/Day/*
+fi
--- .pure/scripts/wallpaper_night.sh
+++ .pure/scripts/wallpaper_night.sh
@@ -1,1 +1,3 @@
-DISPLAY=:0 feh --bg-fill --randomize /home/pure/Wallpapers/Scenery/Night/*
+if [[ $(date +%k) -gt 18 ]]; then
+    DISPLAY=:0 feh --bg-fill --randomize /home/pure/Wallpapers/Scenery/Night/*
+fi
