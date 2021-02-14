a1531a7497e6043a29877d851459180b799eb0bf
PureFunctor <purefunctor@gmail.com>
2021-02-15 00:26:36 +0800

    Fix boolean grouping for wallpaper script

--- .pure/scripts/wallpaper.sh
+++ .pure/scripts/wallpaper.sh
@@ -1,6 +1,6 @@
 if [[ $1 == "day" && $(date +%k) -ge 8 && $(date +%k) -lt 18 ]]; then
     DISPLAY=:0 feh --bg-fill --randomize /home/pure/Wallpapers/Scenery/Day/*
-elif [[ $1 == "night" && $(date +%k) -ge 18 || $(date +%k) -lt 8 ]]; then
+elif [[ $1 == "night" && (( $(date +%k) -ge 18 || $(date +%k) -lt 8 )) ]]; then
     DISPLAY=:0 feh --bg-fill --randomize /home/pure/Wallpapers/Scenery/Night/*
 else
     echo "Usage: wallpaper.sh (day|night)"
