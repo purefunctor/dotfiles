f5ba37a1e1e98b5f523845c6480b49318a7d09c6
PureFunctor <purefunctor@gmail.com>
2021-02-14 20:39:54 +0800

    Create a unified utility for setting wallpapers

--- .pure/scripts/wallpaper_day.sh
+++
@@ -1,3 +0,0 @@
-if [[ $(date +%k) -lt 18 ]]; then
-   DISPLAY=:0 feh --bg-fill --randomize /home/pure/Wallpapers/Scenery/Day/*
-fi
--- .pure/scripts/wallpaper_night.sh
+++
@@ -1,3 +0,0 @@
-if [[ $(date +%k) -gt 18 ]]; then
-    DISPLAY=:0 feh --bg-fill --randomize /home/pure/Wallpapers/Scenery/Night/*
-fi
